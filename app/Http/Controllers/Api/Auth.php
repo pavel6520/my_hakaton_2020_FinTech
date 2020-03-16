65b6712388e500783fa9ec9f44e6e128b1ee4e14
pavel6520 <pavmuz2012@yandex.ru>
2020-03-17 01:14:13 +0300

    add /api/redirect action, return token to user

--- app/Http/Controllers/Api/Auth.php
+++ app/Http/Controllers/Api/Auth.php
@@ -10,4 +10,18 @@ class Auth extends Controller
     public function signin(){
         return redirect('https://github.com/login/oauth/authorize?client_id='.config('global.oauth.client_id'));
     }
+    public function redirect(){
+        $client = new \GuzzleHttp\Client();
+        $response = $client->post('https://github.com/login/oauth/access_token',
+            [
+                'json'=>[
+                    'client_id'=>config('global.oauth.client_id'),
+                    'client_secret'=>config('global.oauth.client_secret'),
+                    'code'=>\request()->code
+                ]
+            ]);
+        parse_str($response->getBody()->getContents(), $res);
+        $token = $res['access_token'];
+        return response()->json($token);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/signin', 'Api\Auth@signin');
+Route::get('/redirect', 'Api\Auth@redirect');
